ecf8f81d9a094125b0fba6ff21d0cb2adafc4299
Supavee <supavee.ru@ku.th>
2019-09-30 12:17:45 +0700

    Add I buy method

--- src/test/java/ku/shop/BuyStepdefs.java
+++ src/test/java/ku/shop/BuyStepdefs.java
@@ -1,9 +1,12 @@
 package ku.shop;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,12 +21,23 @@ public class BuyStepdefs {
         order = new Order();
     }
 
+    @Given("เรามีสินค้าเหล่านี้")
+    public void เรามีสินค้าเหล่านี้(DataTable table) {
+        Map<String,Double> data = table.asMap(String.class, Double.class);
+
+        for (String name : data.keySet()) {
+            double price = data.get(name);
+            catalog.addProduct(name, price);
+        }
+
+    }
+
     @Given("a product (.+) with price (.+) exists")
     public void a_product_with_price_exists(String name, double price) {
         catalog.addProduct(name, price);
     }
 
-    @When("I buy (.+) with quantity (.+)")
+    @When("ฉันซื้อ (.+) with quantity (.+)")
     public void i_buy_with_quantity(String name, int quant) {
         Product prod = catalog.getProduct(name);
         order.addItem(prod, quant);
